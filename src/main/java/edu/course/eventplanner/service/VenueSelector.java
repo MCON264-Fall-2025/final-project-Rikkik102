0a30836f183d37ec962d2a4b8aef0cb0f9d0e5fe
Rivka Kaufman <rkaufman8@student.touro.edu>
2025-12-30 19:48:37 -0500

    updated venue selector

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -7,7 +7,7 @@ public class VenueSelector {
     private final List<Venue> venues;
     public VenueSelector(List<Venue> venues) { this.venues = venues; }
     public Venue selectVenue(double budget, int guestCount) {
-        double currentCost = venues.getFirst().getCost();
+        double currentCost = 999999999;
         Venue chosen = new Venue("Unknown", 0, 0, 0, 0);
         for (Venue venue:venues) {
             if (venue.getCapacity() > guestCount) {
