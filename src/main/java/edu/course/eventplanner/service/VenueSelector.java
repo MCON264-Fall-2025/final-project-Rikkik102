cd348273b20bcb0d57cac96eb92dc5c858fba65a
Rivka Kaufman <rkaufman8@student.touro.edu>
2026-01-06 18:52:33 -0500

    changed venue selector

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -5,32 +5,23 @@ import java.util.*;
 
 public class VenueSelector {
     private final List<Venue> venues;
-    public VenueSelector(List<Venue> venues) { this.venues = venues; }
+    TreeMap<Double, Venue> tm = new TreeMap<>();
+
+    public VenueSelector(List<Venue> venues) { this.venues = venues;
+       for (Venue venue: venues) {
+           tm.put(venue.getCost(), venue);
+       }
+
+
+    }
     public Venue selectVenue(double budget, int guestCount) {
-        double currentCost = 999999999;
-        double currentCapacity = 99999999;
-        Venue chosen = new Venue("Unknown", 0, 0, 0, 0);
-        for (Venue venue:venues) {
-            if (venue.getCapacity() > guestCount) {
-                if (venue.getCost() < budget) {
-                    if (venue.getCost() < currentCost) {
-                        currentCost = venue.getCost();
-                       currentCapacity = venue.getCapacity();
-                        chosen = venue;
-                    }
-                    else if (venue.getCost() == currentCost) {
-                        if (venue.getCapacity() < currentCapacity) {
-                            currentCost = venue.getCost();
-                            currentCapacity = venue.getCapacity();
-                            chosen = venue;
-                        }
-                    }
-                }
+        for (Map.Entry<Double, Venue> entry : tm.entrySet()) {
+            Venue venue = entry.getValue();
+            if (entry.getKey() <= budget &&
+                    venue.getCapacity() >= guestCount) {
+                return venue;
             }
         }
-        if ((chosen.getName().equals("Unknown"))) {
-            return null;
-        }
-        return chosen;
+        return null;
     }
 }
