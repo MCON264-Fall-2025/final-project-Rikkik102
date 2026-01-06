e4420a13f669e3d0bfc332ad15eabb421bed6906
Rivka Kaufman <rkaufman8@student.touro.edu>
2026-01-06 18:11:22 -0500

    Added tiebreaker to venue selector

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -8,13 +8,22 @@ public class VenueSelector {
     public VenueSelector(List<Venue> venues) { this.venues = venues; }
     public Venue selectVenue(double budget, int guestCount) {
         double currentCost = 999999999;
+        double currentCapacity = 99999999;
         Venue chosen = new Venue("Unknown", 0, 0, 0, 0);
         for (Venue venue:venues) {
             if (venue.getCapacity() > guestCount) {
                 if (venue.getCost() < budget) {
                     if (venue.getCost() < currentCost) {
                         currentCost = venue.getCost();
+                       currentCapacity = venue.getCapacity();
                         chosen = venue;
+                    }
+                    else if (venue.getCost() == currentCost) {
+                        if (venue.getCapacity() < currentCapacity) {
+                            currentCost = venue.getCost();
+                            currentCapacity = venue.getCapacity();
+                            chosen = venue;
+                        }
                     }
                 }
             }
