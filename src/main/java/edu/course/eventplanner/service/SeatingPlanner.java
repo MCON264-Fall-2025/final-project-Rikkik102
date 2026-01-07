f784d7f31512ed6b32676056252062519ce9c578
Rivka Kaufman <rkaufman8@student.touro.edu>
2026-01-06 20:06:56 -0500

    started seating

--- src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
+++ src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
@@ -5,10 +5,53 @@ import java.util.*;
 
 public class SeatingPlanner {
     private final Venue venue;
+    HashMap<String, List<Guest>> hm = new HashMap<>();
+    HashMap<Integer, List<Guest>> answer = new HashMap<>();
     public SeatingPlanner(Venue venue) { this.venue = venue; }
     public Map<Integer, List<Guest>> generateSeating(List<Guest> guests) {
         if (venue.getCapacity() < guests.size()) {
             return null;
+        }
+        else {
+            for (Guest guest: guests) {
+                if (hm.containsKey(guest.getGroupTag())) {
+                    hm.get(guest.getGroupTag()).add(guest);
+                }
+                else {
+                    List<Guest> list = new LinkedList<>();
+                    list.add(guest);
+                    hm.put(guest.getGroupTag(), list);
+                }
+            }
+
+            int numOfGroups = hm.size();
+
+            int seats = venue.getSeatsPerTable();
+
+            int maxTables = venue.getTables();
+
+            List<Guest> clique;
+            List<Guest> overflow;
+            List<Guest> tables;
+
+            if (maxTables >= numOfGroups) {
+                Integer currTable = 1;
+                for (Map.Entry<String, List<Guest>> entry : hm.entrySet()) {
+                    String key = entry.getKey();
+                    clique = hm.get(key);
+                    answer.put(currTable, clique);
+                    currTable++;
+                }
+
+            for (Map.Entry<String, List<Guest>> entry : hm.entrySet()) {
+
+                String key = entry.getKey();
+                clique = hm.get(key);
+
+
+                }
+            }
+
         }
 
         return Map.of();
