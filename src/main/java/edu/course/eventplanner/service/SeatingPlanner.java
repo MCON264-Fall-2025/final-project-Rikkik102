1f2604b17c8fbacc18a9a078d428cbb92c24631b
Rivka Kaufman <rkaufman8@student.touro.edu>
2026-01-10 20:39:23 -0500

    added in when there are more groups than tables

--- src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
+++ src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
@@ -7,17 +7,19 @@ public class SeatingPlanner {
     private final Venue venue;
     HashMap<String, List<Guest>> hm = new HashMap<>();
     HashMap<Integer, List<Guest>> answer = new HashMap<>();
-    public SeatingPlanner(Venue venue) { this.venue = venue; }
+
+    public SeatingPlanner(Venue venue) {
+        this.venue = venue;
+    }
+
     public Map<Integer, List<Guest>> generateSeating(List<Guest> guests) {
         if (venue.getCapacity() < guests.size()) {
             return null;
-        }
-        else {
-            for (Guest guest: guests) {
+        } else {
+            for (Guest guest : guests) {
                 if (hm.containsKey(guest.getGroupTag())) {
                     hm.get(guest.getGroupTag()).add(guest);
-                }
-                else {
+                } else {
                     List<Guest> list = new LinkedList<>();
                     list.add(guest);
                     hm.put(guest.getGroupTag(), list);
@@ -25,35 +27,60 @@ public class SeatingPlanner {
             }
 
             int numOfGroups = hm.size();
-
-            int seats = venue.getSeatsPerTable();
-
+            int seatsPerTable = venue.getSeatsPerTable();
             int maxTables = venue.getTables();
 
             List<Guest> clique;
-            List<Guest> overflow;
-            List<Guest> tables;
+            List<Guest> overflow = new ArrayList<>();
+            List<Integer> tablesWithExtraRoom = new ArrayList<>();
 
             if (maxTables >= numOfGroups) {
                 Integer currTable = 1;
+
                 for (Map.Entry<String, List<Guest>> entry : hm.entrySet()) {
-                    String key = entry.getKey();
-                    clique = hm.get(key);
+                    clique = entry.getValue();
+
+                    while (clique.size() > seatsPerTable) {
+                        overflow.add(clique.removeLast());
+                    }
+
                     answer.put(currTable, clique);
+
+                    if (clique.size() < seatsPerTable) {
+                        tablesWithExtraRoom.add(currTable);
+                    }
+
                     currTable++;
                 }
 
-            for (Map.Entry<String, List<Guest>> entry : hm.entrySet()) {
+                if (!overflow.isEmpty() && maxTables > numOfGroups) {
+                    //put extra guests on empty tables
+                    for (Integer i = currTable; i <= maxTables && !overflow.isEmpty(); i++) {
+                        List<Guest> extraGuests = new ArrayList<>();
 
-                String key = entry.getKey();
-                clique = hm.get(key);
+                        while (!overflow.isEmpty() && extraGuests.size() < seatsPerTable) {
+                            extraGuests.add(overflow.removeFirst());
+                        }
 
+                        answer.put(i, extraGuests);
+                    }
+                }
 
+                if (!overflow.isEmpty()) {
+                    // put guests on table with extra room
+                    for (Integer key : tablesWithExtraRoom) {
+                        List<Guest> table = answer.get(key);
+
+                        while (!overflow.isEmpty() && table.size() < seatsPerTable) {
+                            table.add(overflow.removeFirst());
+                        }
+
+                        if (overflow.isEmpty()) break;
+                    }
                 }
             }
 
+            return answer;
         }
-
-        return Map.of();
     }
 }
--- src/test/java/edu/course/eventplanner/serviceTests/SeatingPlannerTest.java
+++ src/test/java/edu/course/eventplanner/serviceTests/SeatingPlannerTest.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SeatingPlannerTest {
     @Test
-    void testSmallHallGenerateSeating() {
+    void testNotEnoughSeats() {
         Venue sands = new Venue("Sands Atlantic", 100000, 1, 50, 10);
         SeatingPlanner sp = new SeatingPlanner(sands);
         List<Guest> guests= new LinkedList<>();
@@ -19,5 +22,83 @@ public class SeatingPlannerTest {
         guests.add(harvey);
         guests.add(deidre);
         sp.generateSeating(guests);
+        assertNull(sp.generateSeating(guests));
+    }
+
+    @Test
+    void testGenerateSeatingWithEnoughTables() {
+        Venue sands = new Venue("Sands Atlantic", 100000, 30, 3, 10);
+        SeatingPlanner sp = new SeatingPlanner(sands);
+        List<Guest> guests= new LinkedList<>();
+        Guest harvey = new Guest("Harvey", "neighbor");
+        Guest deidre = new Guest("Deidre", "family");
+        Guest harveyy = new Guest("Harvey", "neighbor");
+        Guest deidree = new Guest("Deidre", "family");
+        Guest harveyyy = new Guest("Harvey", "neighbor");
+        Guest deidreee = new Guest("Deidre", "family");
+        guests.add(harvey);
+        guests.add(deidre);
+        guests.add(harveyy);
+        guests.add(deidree);
+        guests.add(harveyyy);
+        guests.add(deidreee);
+        Map<Integer, List<Guest>> answer= sp.generateSeating(guests);
+        assertEquals(3, answer.get(1).size());
+        assertEquals(3, answer.get(2).size());
+        assertEquals(2, answer.size());
+    }
+    @Test
+    void testGenerateSeatingWithNotEnoughTables() {
+        Venue sands = new Venue("Sands Atlantic", 100000, 6, 3, 2);
+        SeatingPlanner sp = new SeatingPlanner(sands);
+        List<Guest> guests= new LinkedList<>();
+        Guest harvey = new Guest("Harvey", "neighbor");
+        Guest deidre = new Guest("Deidre", "family");
+        Guest harveyy = new Guest("Harvey", "neighbor");
+        Guest deidree = new Guest("Deidre", "family");
+        Guest harveyyy = new Guest("Harvey", "neighbor");
+        Guest carmen = new Guest("Carmen", "family");
+        guests.add(harvey);
+        guests.add(deidre);
+        guests.add(harveyy);
+        guests.add(deidree);
+        guests.add(harveyyy);
+        guests.add(carmen);
+        Map<Integer, List<Guest>> answer= sp.generateSeating(guests);
+        assertEquals(3, answer.size());
+    }
+    @Test
+    void testGenerateSeatingWithSplitGroupTables() {
+        Venue sands = new Venue("Sands Atlantic", 100000, 6, 2, 3);
+        SeatingPlanner sp = new SeatingPlanner(sands);
+        List<Guest> guests= new LinkedList<>();
+        Guest harvey = new Guest("Harvey", "neighbor");
+        Guest deidre = new Guest("Deidre", "family");
+        Guest harveyy = new Guest("Harvey", "neighbor");
+        Guest deidree = new Guest("Deidre", "family");
+        Guest harveyyy = new Guest("Harvey", "neighbor");
+        Guest carmen = new Guest("Carmen", "neighbor");
+        guests.add(harvey);
+        guests.add(deidre);
+        guests.add(harveyy);
+        guests.add(deidree);
+        guests.add(harveyyy);
+        guests.add(carmen);
+        Map<Integer, List<Guest>> answer= sp.generateSeating(guests);
+        assertEquals(2, answer.size());
+        boolean neighborOnFamilyTable = false;
+        for (List<Guest> table : answer.values()) {
+            boolean hasFamily = table.stream()
+                    .anyMatch(g -> g.getGroupTag().equals("family"));
+
+            boolean hasNeighbor = table.stream()
+                    .anyMatch(g -> g.getGroupTag().equals("neighbor"));
+
+            if (hasFamily && hasNeighbor) {
+                neighborOnFamilyTable = true;
+                break;
+            }
+        }
+        assertTrue(neighborOnFamilyTable);
     }
 }
